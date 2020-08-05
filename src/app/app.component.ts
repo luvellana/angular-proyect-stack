01082ebc96dec1647cbc3c7abcda74df70d1176d
luvellana <luciananunezl99@gmail.com>
2020-08-05 15:56:59 -0400

    ejercicios en clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, fromEvent } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'curso-angular';
+
+  ngOnInit(){
+    const array = from( [0, 1, 2] );
+    array.subscribe(a => console.log('item: ', a))
+
+    const aux = fromEvent(document, 'mousemove');
+
+    aux.subscribe((a:a) => console.log('event: ', a.clientX + ', ' + a.clientY))
+  }
+  
 }
