ba8eccf0b299ec7cd3163390c97365a343c84171
luvellana <luciananunezl99@gmail.com>
2020-08-07 16:36:10 -0400

    input, Output

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test1',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test1.component.css']
 })
 export class Test1Component implements OnInit {
+  @Input() name : string;
+  @Input() age : number;
+  @Input() description : string;
+
+  @Output() clickSave = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  OnClickSave(){
+    this.clickSave.emit({
+      name: this.name,
+      age: this.age,
+      description: this.description
+    });
   }
 
 }
