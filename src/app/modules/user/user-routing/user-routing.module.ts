f401a33bc07fa8b55621f6972b214b684ea724a7
luvellana <luciananunezl99@gmail.com>
2020-08-24 15:27:19 -0400

    routing with modules

--- src/app/modules/user/user-routing/user-routing.module.ts
+++ src/app/modules/user/user-routing/user-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from '../components/user/user.component';
+import { Us1Component } from '../components/us1/us1.component';
+import { Us2Component } from '../components/us2/us2.component';
 
 const routes: Routes = [
-  {path: '', component: UserComponent}
+  {
+    path: '',
+    component: UserComponent,
+    children: [
+      {path: 'user1', component: Us1Component},
+      {path: 'user2', component: Us2Component}
+    ]
+  }
 ];
 
 @NgModule({
@@ -11,4 +20,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class UserRoutingModule { }
+export class UserRoutingModule {
+}
