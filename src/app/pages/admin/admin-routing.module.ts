4511d760cd99054c7beccb0663fb42b9d9148bfa
luvellana <luciananunezl99@gmail.com>
2020-08-25 17:17:22 -0400

    tarea

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminComponent } from '../../components/admin/admin.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   {path: '', component: AdminComponent}
