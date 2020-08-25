e97021f0e2c30575804d80f81b7c4582f297fb53
luvellana <luciananunezl99@gmail.com>
2020-08-25 16:47:28 -0400

    2 files modified, 10 added, 25 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
-import {AdminComponent} from './components/admin/admin.component';
-import {AboutComponent} from './components/about/about.component';
-import {Home1Component} from './components/home1/home1.component';
-import {Home2Component} from './components/home2/home2.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-    children: [
-      {path: 'home1', component: Home1Component},
-      {path: 'home2', component: Home2Component}
-    ]
-  },
-  {path: 'admin', component: AdminComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)}
 ];
 
 @NgModule({
@@ -25,5 +12,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule { 
-*
+export class AppRoutingModule { }
