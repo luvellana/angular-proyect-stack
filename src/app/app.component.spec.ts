0c5e0d5413b84b77e9f4ffa19619e3544b0eecfe
luvellana <luciananunezl99@gmail.com>
2020-08-26 23:35:42 -0400

    5 files modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'curso-angular'`, () => {
+  it(`should have as title 'proyecto-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('curso-angular');
+    expect(app.title).toEqual('proyecto-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('curso-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('proyecto-angular app is running!');
   });
 });
