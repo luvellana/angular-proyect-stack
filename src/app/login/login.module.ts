ca538bf5cc27ba61b2666469a03370ca94c5916f
luvellana <luciananunezl99@gmail.com>
2020-08-31 21:46:19 -0400

    Reactive, template-Driven forms Tarea 31/08

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { NgElseDirective } from '../directives/ng-else.directive';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { NgElseDirective } from '../directives/ng-else.directive';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule
   ]
 })
 export class LoginModule { }
