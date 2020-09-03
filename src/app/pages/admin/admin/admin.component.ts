0bbe3cb5267d899e163052ad7f886c9f7e09eb23
luvellana <luciananunezl99@gmail.com>
2020-09-03 13:46:37 -0400

    tareas 02/09

--- src/app/pages/admin/admin/admin.component.ts
+++ src/app/pages/admin/admin/admin.component.ts
@@ -1,19 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ProductService} from '../../../services/product.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-admin',
-  templateUrl: './admin.component.html'
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
+
+  products = [];
 
   productForm: FormGroup;
+
+  productSubs: Subscription;
+  productGetSubs: Subscription;
 
   // nameControl = new FormControl();
 
   constructor(private formBuilder: FormBuilder,
               private productService: ProductService) {
+
+    this.productGetSubs = this.productService.getProducts().subscribe(res => {
+
+      // [1,2,3,4,5,6];
+      // {{key:1 },{key: 2},{key: 1},{key: 1},{key: 1},{key: 1},{key: 1}}
+      // Object.entries(res) [ [key, 1], [key, 2] , .......              ];
+
+      console.log('RESPUESTA: ', res);
+      console.log('RESPUESTA: ', Object.entries(res));
+
+      Object.entries(res).map(p => this.products.push(p[1]));
+
+    });
+
   }
 
   ngOnInit(): void {
@@ -35,15 +56,20 @@ export class AdminComponent implements OnInit {
   onEnviar2(): void {
     console.log('FORM GROUP: ', this.productForm.value);
 
-    this.productService.addProduct(this.productForm.value).subscribe(
+    this.productSubs = this.productService.addProduct(this.productForm.value).subscribe(
       res => {
-      console.log('RESP: ', res);
+        console.log('RESP: ', res);
       },
       err => {
         console.log('ERROR DE SERVIDOR');
       }
     );
 
+  }
+
+  ngOnDestroy(): void {
+    this.productSubs ? this.productSubs.unsubscribe() : '';
+    this.productGetSubs ? this.productGetSubs.unsubscribe() : '';
   }
 
 }
