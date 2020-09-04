0b152c4a561739ee840d88d7180bf96971545b49
luvellana <luciananunezl99@gmail.com>
2020-09-04 18:13:43 -0400

    Examen finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component , OnInit} from '@angular/core';
+import { Component , OnInit, OnDestroy} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {ProductService} from './product.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,54 +9,75 @@ import { Component , OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'curso-angular';
-  stockTot = 0;
-  conciertos=[
-{nombre: "bad bunny", 
-stock:1 , 
-habilitado: true, 
-tipo: "internacional"
-},
-{nombre: "karkas", 
-stock: 2 , 
-habilitado: true, 
-tipo: "nacional"
-},
-{nombre: "ozuna", 
-stock: 0 , 
-habilitado: false, 
-tipo: "internacional"
-},
-{nombre: "PK2", 
-stock: 45 , 
-habilitado: true, 
-tipo: "nacional"},
-{nombre: "Octavia", 
-stock: 20 , 
-habilitado: true, 
-tipo: "nacional"},
-{nombre: "julieta venegas", 
-stock: 1 , 
-habilitado: true, 
-tipo: "internacional"},
-{nombre: "rumba 7", 
-stock: 4 , 
-habilitado: true, 
-tipo: "nacional"},
-{nombre: "metalica", stock: 0 , habilitado: false, tipo: 
-"internacional"},
-{nombre: "alcoholica", stock: 33 , habilitado: true, tipo: "nacional"},
-{nombre: "juanes", stock: 0 , habilitado: false, tipo: "internacional"}
-]
+  products = [];
+
+  productForm: FormGroup;
+
+  productSubs: Subscription;
+  productGetSubs: Subscription;
+  productDeleteSubs: Subscription;
+  productUpdateSubs: Subscription;
+  idEdit: any;
+
+   constructor(private formBuilder: FormBuilder,
+              private productService: ProductService) {
+  }
 
 ngOnInit(){
-  for(let concert of this.conciertos){
-    this.stockTot += concert.stock
-  }
+  this.loadProduct();
+
+    this.productForm = this.formBuilder.group({
+      imageUrl: '',
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      price: '',
+      stock: '',
+      type: ['', [Validators.required]]
+    });
 }
 
-vender(){
-  this.stockTot--
-}
+loadProduct(): void {
+    this.products = [];
+    this.productGetSubs = this.productService.getProducts().subscribe(res => {
+      Object.entries(res).map((p: any) => this.products.push({id: p[0], ...p[1]}));
+    });
+  }
+
+onDelete(id: any): void {
+    this.productDeleteSubs = this.productService.deleteProduct(id).subscribe(
+      res => {
+        this.loadProduct();
+      }
+      
+    );
+  }
+ onEdit(product): void {
+    this.idEdit = product.id;
+    this.productForm.patchValue(product);
+  }
+
+  onUpdateProduct(): void {
+    this.productUpdateSubs = this.productService.updateProduct(this.idEdit, this.productForm.value).subscribe(
+      res => {
+        this.loadProduct();
+      }
+    );
+  }
+
+  onCreate(): void {
+    this.productSubs = this.productService.addProduct(this.productForm.value).subscribe(
+      res => {
+        this.loadProduct();
+      }
+    );
+
+  }
+
+  ngOnDestroy(): void {
+    this.productSubs ? this.productSubs.unsubscribe() : '';
+    this.productGetSubs ? this.productGetSubs.unsubscribe() : '';
+    this.productDeleteSubs ? this.productDeleteSubs.unsubscribe() : '';
+    this.productUpdateSubs ? this.productUpdateSubs.unsubscribe() : '';
+  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ConciertoComponent } from './concierto/concierto.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ConciertoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
